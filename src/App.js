77769dd65383b9d5b38cc5e119381790e970599f
github username <github email address>
2019-06-14 14:31:55 -0400

    auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 // import { Route, Switch } from 'react-router-dom';
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 
 import './App.css';
 
 class App extends React.Component {
+
+  logOut = () => {
+    localStorage.removeItem("token")
+    this.props.setCurrentUser({currentUser: null})
+    this.props.history.push('/')
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem("token")
+
+    if (token) {
+      fetch("http://localhost:3001/auto_login", {
+        headers: {
+          "Authorization": token
+        }
+      })
+      .then(res => res.json())
+      .then((response) => {
+        this.props.setCurrentUser(response)
+      })
+    }
+  }
 
   render () {
     return (
@@ -16,4 +38,18 @@ class App extends React.Component {
 
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setCurrentUser: (user) => {
+      dispatch({type: "SET_CURRENT_USER", payload: user})
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
